800c32b4a9f656b2433e63d786eb8816c9a15a66
rodrigo.guilherme <AB1210160@BRCLDDEV652>
2018-05-02 16:26:33 -0300

    configured dbcontext

---
+++ eStore.Data/Migrations/20180502192213_init.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eStore.Data.EF.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produto",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Nome = table.Column<string>(nullable: true),
+                    Descricao = table.Column<string>(nullable: true),
+                    ValorUnitario = table.Column<double>(nullable: false),
+                    Categoria = table.Column<int>(nullable: false),
+                    Altura = table.Column<double>(nullable: false),
+                    Largura = table.Column<double>(nullable: false),
+                    Profundidade = table.Column<double>(nullable: false),
+                    Peso = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produto", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Nome = table.Column<string>(nullable: true),
+                    Sobrenome = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Senha = table.Column<string>(nullable: true),
+                    Nascimento = table.Column<DateTime>(nullable: false),
+                    Logradouro = table.Column<string>(nullable: true),
+                    Numero = table.Column<int>(nullable: false),
+                    CEP = table.Column<string>(nullable: true),
+                    Bairro = table.Column<string>(nullable: true),
+                    Complemento = table.Column<string>(nullable: true),
+                    Cidade = table.Column<string>(nullable: true),
+                    UF = table.Column<string>(nullable: true),
+                    Perfil = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Carrinho",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    UsuarioId = table.Column<Guid>(nullable: true),
+                    ValorTotal = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carrinho", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Carrinho_Usuario_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pedido",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    UsuarioId = table.Column<Guid>(nullable: true),
+                    Status = table.Column<int>(nullable: false),
+                    ValorTotal = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pedido", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pedido_Usuario_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarrinhoItem",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CarrinhoId = table.Column<Guid>(nullable: false),
+                    ProdutoId = table.Column<Guid>(nullable: true),
+                    Quantidade = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarrinhoItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CarrinhoItem_Carrinho_CarrinhoId",
+                        column: x => x.CarrinhoId,
+                        principalTable: "Carrinho",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CarrinhoItem_Produto_ProdutoId",
+                        column: x => x.ProdutoId,
+                        principalTable: "Produto",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pagamento",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Valor = table.Column<double>(nullable: false),
+                    Status = table.Column<int>(nullable: false),
+                    Forma = table.Column<int>(nullable: false),
+                    QuantidadeParcelas = table.Column<int>(nullable: false),
+                    PedidoId = table.Column<Guid>(nullable: true),
+                    Logradouro = table.Column<string>(nullable: true),
+                    Numero = table.Column<int>(nullable: false),
+                    CEP = table.Column<string>(nullable: true),
+                    Bairro = table.Column<string>(nullable: true),
+                    Complemento = table.Column<string>(nullable: true),
+                    Cidade = table.Column<string>(nullable: true),
+                    UF = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pagamento", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pagamento_Pedido_PedidoId",
+                        column: x => x.PedidoId,
+                        principalTable: "Pedido",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PedidoItem",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    PedidoId = table.Column<Guid>(nullable: false),
+                    Quantidade = table.Column<int>(nullable: false),
+                    ProdutoId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PedidoItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PedidoItem_Pedido_PedidoId",
+                        column: x => x.PedidoId,
+                        principalTable: "Pedido",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PedidoItem_Produto_ProdutoId",
+                        column: x => x.ProdutoId,
+                        principalTable: "Produto",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carrinho_UsuarioId",
+                table: "Carrinho",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarrinhoItem_CarrinhoId",
+                table: "CarrinhoItem",
+                column: "CarrinhoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarrinhoItem_ProdutoId",
+                table: "CarrinhoItem",
+                column: "ProdutoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pagamento_PedidoId",
+                table: "Pagamento",
+                column: "PedidoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pedido_UsuarioId",
+                table: "Pedido",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PedidoItem_PedidoId",
+                table: "PedidoItem",
+                column: "PedidoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PedidoItem_ProdutoId",
+                table: "PedidoItem",
+                column: "ProdutoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarrinhoItem");
+
+            migrationBuilder.DropTable(
+                name: "Pagamento");
+
+            migrationBuilder.DropTable(
+                name: "PedidoItem");
+
+            migrationBuilder.DropTable(
+                name: "Carrinho");
+
+            migrationBuilder.DropTable(
+                name: "Pedido");
+
+            migrationBuilder.DropTable(
+                name: "Produto");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
